1f369a47723ac7160358102d6a9e6d0faa885cef
Evelia Coss <y0kocrozz@gmail.com>
2026-05-26 07:26:39 -0600

    agregar ejemplo de funcion y documentacion con roxygen

---
+++ R/time.R
@@ -0,0 +1,23 @@
+#' Title
+#'
+#' @param language
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+what_time <- function(language = "es") {
+
+  if (!language %in% c("es", "en")) {
+    stop("Either choose 'es' or 'en' as a language.")
+  }
+
+  time <- format(Sys.time(), "%H:%M")
+
+  switch(
+    language,
+    es = sprintf("¡Ahora son las %s!", time),
+    en = sprintf("It is %s now!", time)
+  )
+
+}
